c9c3a9ad1f64fc7fc707b1da0d9611937ec462e7
Tevtongermany <alexm3350@gmail.com>
2023-05-21 17:02:44 +0200

    got no energy anymore
    last Commit for the day

--- C# Version/YT2File/YT2File/ViewModels/MainWindowViewModel.cs
+++ C# Version/YT2File/YT2File/ViewModels/MainWindowViewModel.cs
@@ -14,20 +14,34 @@ using System.Threading;
 using System.Net.Http;
 using Serilog;
 using System.Diagnostics;
+using System.Text.Json;
+using System.Security.Policy;
 
 namespace YT2File.ViewModels;
-
+class YTData
+{
+    public List<VideoItem> Items { get; set; }
+}
+class VideoItem
+{
+    public Snippet Snippet { get; set; }
+}
+class Snippet
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
 public partial class MainWindowViewModel : ObservableObject
 {
     // Command Stuff
     public ICommand TestCommand { get; }
-    public ICommand StartDownloadCommand { get; }
+    public IAsyncRelayCommand DownloadCommand { get; }
 
     // Starts when the viewmodel is initialized
     public MainWindowViewModel()
     {
         TestCommand = new RelayCommand(test);
-        StartDownloadCommand = new RelayCommand(Download);
+        DownloadCommand = new AsyncRelayCommand(StartDownload);
         
     }
     // Variables
@@ -42,6 +56,16 @@ public partial class MainWindowViewModel : ObservableObject
             OnPropertyChanged();
         }
     }
+    private string _yttitle = string.Empty;
+    public string YTTitle
+    {
+        get { return _yttitle; }
+        set
+        {
+            _yttitle = value;
+            OnPropertyChanged(nameof(YTTitle));
+        }
+    }
 
     // Functions
     [RelayCommand]
@@ -51,18 +75,15 @@ public partial class MainWindowViewModel : ObservableObject
     }
 
     [RelayCommand]
-    public void Download()
-    {
-        Thread t = new Thread(new ThreadStart(StartDownload));
-        t.Start();
-    }
-    public void StartDownload()
+
+    public async Task StartDownload()
     {
         switch (Format)
         {
             case 0:
                 try
                 {
+                    
                     ProcessStartInfo startInfo = new ProcessStartInfo();
                     startInfo.FileName = "Deps/yt-dlp.exe";
                     startInfo.Arguments = $"-f mp4 --ffmpeg-location Deps/ffmpeg/bin {lastURL}";
@@ -106,4 +127,29 @@ public partial class MainWindowViewModel : ObservableObject
 
     }
 
+    private async Task GetMetaData(string Url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(Url);
+                response.EnsureSuccessStatusCode(); // Throw an exception if the response is not successful
+
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                // Deserialize the JSON into an object
+                var data = JsonSerializer.Deserialize<YTData>(jsonResponse);
+
+                // Access the properties of the data object
+                YTTitle = data.Items[0].Snippet.Title;
+                // ...
+
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
 }
